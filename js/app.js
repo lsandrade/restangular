b77ec31d26b791c29626040bcd2e89009c0e5f8c
Luan Andrade <luansandrade92@hotmail.com>
2017-03-23 18:00:39 -0300

    restangular não pegando snippers

--- js/app.js
+++ js/app.js
@@ -13,5 +13,26 @@ app
         });
 })
 
+// Restangular config
+.config(function(RestangularProvider) {
+    RestangularProvider.setBaseUrl(
+        'http://127.0.0.1:8000/');
+})
+
+.config(function(RestangularProvider) {
+    // add a response intereceptor
+    RestangularProvider.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
+      var extractedData;
+      // .. to look for getList operations
+      if (operation === "getList") {
+        // .. and handle the data and meta data
+        extractedData = data.snippets;
+      } else {
+        extractedData = data;
+      }
+      return extractedData;
+    });
+})
+
 
 ;
--- js/controllers.js
+++ js/controllers.js
@@ -2,6 +2,39 @@ app
 
 .controller('HomeCtrl', function ($scope, Restangular) {
     $scope.msg = "Hello";
+    var url = 'http://127.0.0.1/';
+
+    // First way of creating a Restangular object.
+    // Just saying the base URL
+    var baseSnippets = Restangular.all('snippets/');
+
+    // This will query /snippets and return a promise
+    baseSnippets.getList().then(function(snippets) {
+      $scope.allSnippets = snippets;
+      console.log($scope.allSnippets);
+    });
+
+
+    // Does GET /snippets
+    // Returns an empty array by default. Once a value is returned from the server
+    // that array is filled with those values. So you can use this in your template
+    $scope.snippets = Restangular.all('snippets/')
+        .getList().$object;
+    console.log($scope.snippets);
+
+    /* Desconsidera daqui pra baixo
+    var newAccount = {name: "Luan's account"};
+
+    // POST /accounts
+    baseAccounts.post(newAccount);
+
+    // GET to URL
+    Restangular.allUrl('googlers', url).getList();
+
+    // GET to URL/1
+    Restangular.oneUrl('googlers', url+'1').get();
+
+*/
 })
 
 ;
