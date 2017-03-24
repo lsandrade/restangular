85c1645cd4ad8ac2d2d37fdfa8f7d9c775b59045
Luan Andrade <luansandrade92@hotmail.com>
2017-03-24 11:57:55 -0300

    get one user

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,6 @@
 app
 
-.controller('HomeCtrl', function ($scope, Restangular) {
+.controller('HomeCtrl', function ($scope, Restangular, $q) {
     $scope.msg = "Hello";
 
     // First way of creating a Restangular object.
@@ -16,21 +16,28 @@ app
     // Does GET /snippets
     // Returns an empty array by default. Once a value is returned from the server
     // that array is filled with those values. So you can use this in your template
-    $scope.snippets = Restangular.all('users')
+    $scope.users = Restangular.all('users')
         .getList().$object;
-    console.log($scope.snippets);
+    console.log($scope.users);
 
     var newUser = {username: "admin"+ Math.random()};
 
     // POST /accounts
-    baseUsers.post(newUser);
-/*
+    //baseUsers.post(newUser);
+
     // GET to URL
-    Restangular.allUrl('googlers', url).getList();
+    //Restangular.allUrl('googlers', url).getList();
 
     // GET to URL/1
-    Restangular.oneUrl('googlers', url+'1').get();
-*/
+    //Restangular.oneUrl('googlers', Restangular.baseUrl+'1').get();
+
+    // Just ONE GET to /users/1
+    Restangular.one('users/',1).get().then(function(user){
+        console.log(user);
+    });
+
+    // Just ONE GET to /accounts/123/buildings
+//    Restangular.one('accounts', 123).getList('buildings')
 })
 
 ;
